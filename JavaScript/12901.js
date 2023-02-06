2bd8e5e2f5e0c2e772905fe721f241505abf3f32
nds95 <nds95@kakao.com>
2023-02-06 15:05:52 +0900

    add Algorithm

---
+++ JavaScript/12901.js
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12901
+
+const a = 5;
+const b = 24;
+
+function solution(a, b) {
+  const date = new Date(`2016-${a}-${b}`);
+
+  return date.toString().split(" ")[0].toUpperCase();
+}
+
+console.log(solution(a, b));
