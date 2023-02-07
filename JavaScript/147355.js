f4c781013b44d35a78402b35175b34b49372d823
nds95 <nds95@kakao.com>
2023-02-07 15:21:48 +0900

    add Algorithm

---
+++ JavaScript/147355.js
@@ -0,0 +1,24 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/147355
+
+const t = "500220839878";
+const p = "7";
+
+function solution(t, p) {
+  let answerList = [];
+
+  for (let i = 0, len = t.length; i < len; i++) {
+    let word = "";
+    for (let j = i, jlen = j + p.length; j < jlen; j++) {
+      if (t[j] === undefined) continue;
+      word += t[j];
+      if (word.length === p.length && Number(word) <= Number(p)) {
+        console.log(word);
+        answerList.push(word);
+      }
+    }
+  }
+
+  return answerList.length;
+}
+
+console.log(solution(t, p));
