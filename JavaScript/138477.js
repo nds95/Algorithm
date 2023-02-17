082f3aa71cd7c23d0c735ee3a768b3cff4a3f8d1
남두성 <nds95@kakao.com>
2023-02-17 23:56:42 +0900

    add Algorithm

--- JavaScript/138477.js
+++ JavaScript/138477.js
@@ -3,16 +3,23 @@
 const k = 3;
 const score = [10, 100, 20, 150, 1, 100, 200];
 
-const solution = (k, score) => {
-  let answer = [];
-  let hallOfFame = [];
-  for (let i = 0, len = score.length; i < len; i++) {
-    if (hallOfFame.length < k) hallOfFame.push(score[i]);
-    // score가 명예의 전당의 최저 점수보다 클 때
-    if (score[i] > Math.min(...hallOfFame)) {
-    }
-    answer.push(Math.min(...hallOfFame));
-  }
-};
+function solution(k, score) {
+  // return Math.min(score);
+  // let answer = [];
+  // let hallOfFame = [];
+  // for (let i = 0, len = score.length; i < len; i++) {
+  //   // 명예의 전당에 k보다 적은 수가 있을 때 무조건 push
+  //   if (hallOfFame.length < k) {
+  //     hallOfFame.push(score[i]);
+  //   }
+  //   // score가 명예의 전당의 최저 점수보다 클 때
+  //   if (hallOfFame.length > k && score[i] > Math.min(...hallOfFame)) {
+  //     score = score.sort((a, b) => a - b);
+  //     score[0] = score[i];
+  //   }
+  //   answer.push(Math.min(...hallOfFame));
+  // }
+  // return answer;
+}
 
 console.log(solution(k, score));
