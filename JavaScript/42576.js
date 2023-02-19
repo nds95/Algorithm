402b64f8138b7b69c3af0a6c520d7d1b7b254a24
남두성 <nds95@kakao.com>
2023-02-19 22:40:04 +0900

    modified Algorithm

--- JavaScript/42576.js
+++ JavaScript/42576.js
@@ -1,42 +1,16 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42576
 
-const participant = ["mislav", "stanko", "mislav", "ana"];
-const completion = ["stanko", "ana", "mislav"];
+const participant = ["leo", "kiki", "eden"];
+const completion = ["eden", "kiki"];
 
 const solution = (participant, completion) => {
-  let completionObj = {};
-  for (let i = 0, len = participant.length; i < len; i++) {
-    const findedIndex = participant[i];
-    // completion에 포함되지 않으면 미완주 선수이므로 바로 return
-    if (!completion.includes(findedIndex)) return findedIndex;
-
-    // dict에 하나씩 추가
-    if (completionObj.hasOwnProperty(findedIndex)) {
-      completionObj[findedIndex]++;
-      continue;
-    }
-
-    completionObj[findedIndex] = 1;
-  }
-  console.log(completionObj);
-
+  participant.sort();
+  completion.sort();
   for (let i = 0, len = completion.length; i < len; i++) {
-    if (completionObj[completion[i]]) {
-      completionObj[completion[i]]--;
-    }
+    console.log(participant[i], completion[i]);
+    if (participant[i] !== completion[i]) return participant[i];
   }
-
-  for (const property in completionObj) {
-    if (completionObj[property] === 1) {
-      return property;
-    }
-  }
-  // participant.forEach((value) => {
-  //   if (completion.includes(value)) {
-  //     if (completionObj.hasOwnProperty(value)) completionObj[value] += 1;
-  //     completionObj[value] = 1;
-  //   }
-  // });
+  return participant[participant.length - 1];
 };
 
 console.log(solution(participant, completion));
