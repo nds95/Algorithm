da808fb97c751ba8e59e2ab682912e5471f3a59c
남두성 <nds95@kakao.com>
2023-03-13 18:13:59 +0900

    add Algorithm

---
+++ JavaScript/161990.js
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/161990
+
+const wallpaper = [".#...", "..#..", "...#."];
+
+const solution = (wallpaper) => {
+  let anaswer = [];
+  return anaswer;
+};
+
+console.log(solution(wallpaper));
