3a4f06fdc5129649c3983dc3589f97eed31f1e53
남두성 <nds95@kakao.com>
2023-03-21 20:38:13 +0900

    add Algorithm

---
+++ JavaScript/161989.js
@@ -0,0 +1,22 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/161989
+
+const n = 4;
+const m = 1;
+const section = [1, 2, 3, 4];
+
+const solution = (n, m, section) => {
+  let answer = 0;
+  let painedSection = 0;
+  for (let i = 0, len = section.length; i < len; i++) {
+    // 이미 페인트칠 한 구역이면 continue;
+    if (painedSection >= section[i]) continue;
+    console.log(painedSection);
+
+    const paintingSection = section[i] + m - 1;
+    painedSection = paintingSection;
+    answer++;
+  }
+  return answer;
+};
+
+console.log(solution(n, m, section));
