07a6d947d2c218cd07b646d7f1dc68d9426b4d6b
남두성 <nds95@kakao.com>
2023-03-23 14:35:26 +0900

    add Algorithm

---
+++ JavaScript/12951.js
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12951
+
+const s = "for     the last week";
+
+const solution = (s) => {
+  let reg = new RegExp(/\s+/, "g");
+  s = s.toLowerCase().split(" ");
+
+  console.log(s);
+  for (let i = 0, len = s.length; i < len; i++) {
+    if (!s[i]) continue;
+    let str = s[i].split("");
+    console.log(str);
+    str.splice(0, 1, str[0].toUpperCase());
+    s[i] = str.join("");
+  }
+
+  return s.join(" ");
+};
+
+console.log(solution(s));
